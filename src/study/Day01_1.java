a1690b9382b3c8dc867b95b514a56817088a1eca
seokc <cshjames315@gmail.com>
2022-07-25 20:35:35 +0900

    Day01

---
+++ src/study/Day01_1.java
@@ -0,0 +1,21 @@
+package study;
+
+public class Day01_1 {
+
+    public static void main(String[] args) {
+
+        // println(): 출력후 줄바꿈
+        System.out.println("Hello World!");
+
+        // print(): 일반적인 출력
+        System.out.print("문자열값\n");       // 문자열: String
+
+        // format: 형식 지정
+        // System.out.printf("형식지정자", "출력값");
+        System.out.printf("%c ", 65);         // 문자 : Character
+        System.out.printf("%d ", 100);        // 정수 : Decimal
+        System.out.printf("%f", 3.14);        // 실수 : Float
+
+    }
+
+}
---
+++ src/study/Day01_2.java
@@ -0,0 +1,25 @@
+package study;
+
+// 입력기능을 코드에 연결
+import java.util.Scanner;
+
+public class Day01_2 {
+
+    public static void main(String[] args) {
+
+        // Scanner를 sc라는 변수로 사용
+        Scanner sc = new Scanner(System.in);
+
+        // 문자열로 입력받기
+        System.out.println(sc.next());
+        // 정수로 입력받기
+        System.out.println(sc.nextInt());
+        // 실수로 입력받기
+        System.out.println(sc.nextFloat());
+        // 줄이 바뀔때까지 입력받기
+        sc.nextLine(); // Enter 입력으로 생긴 '\n' 제거
+        System.out.println(sc.nextLine());
+
+    }
+
+}
